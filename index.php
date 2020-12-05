a07154b6750ecb47759126c18b339596639d3396
kaylamuraoka <kmurs98@gmail.com>
2020-12-04 14:48:17 -1000

    added connection to ClearDB from PHP

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 echo "Hello world, this is my first heroku app";
 
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
