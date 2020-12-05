9ef510dcceaa43752e36897f75f2cbac4c78736d
kaylamuraoka <kmurs98@gmail.com>
2020-12-04 14:50:29 -1000

    testing db connection

--- index.php
+++ index.php
@@ -10,5 +10,12 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
+// Create connection
 $conn = new mysqli($server, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+  echo "Connected successfully";
 ?>
